9481e0a4a00c995b8cd85b823084a7789e3aaa97
Marcos <marcosvazq033@gmail.com>
2023-12-21 17:37:18 +0100

    d

---
+++ OdooProject/lista_tareas/mis_modulos/lista_tareas/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "lista_tareas",
+    'summary': """
+    Sencilla Lista de tareas""",
+    'description': """
+    Sencilla lista de tareas utilizadas para crear un nuevo módulo con un nuevo
+    modelo de datos
+    """,
+    'author': "Sergi García",
+    'website': "https://apuntesfpinformatica.es",
+    #Indicamos que es una aplicación
+    'application': True,
+    # En la siguiente URL se indica qué categorías pueden usarse
+    # https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Vamos a utilizar la categoría Productivity
+    'category': 'Productivity',
+    'version': '0.1',
+    # Indicamos lista de módulos necesarios para que este funcione correctamente
+    # En este ejemplo solo depende del módulo "base"
+    'depends': ['base'],
+
+    # Esto siempre se carga
+    'data': [
+        # Este primero indica la politica de acceso del módulo
+        'security/ir.model.access.csv',
+        # Cargamos las vistas y las plantillas
+        'views/views.xml',
+    ]
+}
