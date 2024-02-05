0f3e5014006d85eefca9a84d8c5df217d3c90c2a
Marcos <marcosvazq033@gmail.com>
2024-02-05 10:59:48 +0100

    add

--- OdooProject/instituto/__manifest__.py
+++ OdooProject/instituto/__manifest__.py
@@ -1,26 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name': 'Instituto',  # Nombre de la aplicación
-    'version': '1.0',  # Versión de la aplicación
-    'category': 'Education',  # Categoría de la aplicación
-    'summary': 'Gestión de alumnado, empresas y tutorías FCT',  # Resumen de la aplicación
-    'sequence': 10,  # Orden de la aplicación en el menú
-    'license': 'AGPL-3',  # Licencia bajo la que se distribuye la aplicación
-    'author': 'Marcos Vazquez',  # Autor de la aplicación
-    'depends': ['base'],  # Módulos de los que depende la aplicación
-    'data': [  # Archivos de datos que se cargarán con la aplicación
-        'security/groups.xml',
-        'security/tutoriafct.xml',
-        'security/alumnado.xml',
-        'security/empresa.xml',
-        'views/menu_principal.xml',
-        'views/tutoriafct/tutoriafct_view.xml',
-        'views/tutoriafct/menu.xml',
-        'views/alumnado/alumnado_view.xml',
-        'views/alumnado/menu.xml',
-        'views/empresa/empresa_view.xml',
-        'views/empresa/menu.xml',
+    'name': 'Instituto',
+    'version': '1.0',
+    'category': 'Technical',
+    'description': 'Módulo instituto',
+    'author': 'DM2',
+    'summary': 'Practicas de alumnos de FP',
+    'depends': [
+        'base',
     ],
-    'installable': True,  # Si la aplicación puede ser instalada
-    'application': True,  # Si el módulo es una aplicación
-    'auto_install': False,  # Si la aplicación se instala automáticamente
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/instituto_alumno_views.xml',
+        'views/instituto_practicas_views.xml',
+        'views/instituto_valoracion_views.xml',
+        'views/instituto_empresa_views.xml',
+        'views/instituto_menus.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
 }
